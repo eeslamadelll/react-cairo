6a521ff7008fccfc7aabdce584493f48c8d52c11
Eslam Adel <eslamadel@MacBook-Pro-3.local>
2022-08-01 05:37:13 +0200

    fix: vercel

--- apps/nextjs-app/pages/index.tsx
+++ apps/nextjs-app/pages/index.tsx
@@ -18,7 +18,7 @@ export function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome nextjs-app 👋
+              Vercel 👋
             </h1>
           </div>
 
