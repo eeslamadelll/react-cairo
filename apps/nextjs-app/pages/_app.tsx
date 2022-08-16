26f9903036accf448d550fe69b48fffe4380987a
Eslam Adel <eslamadel@MacBook-Pro-3.local>
2022-08-16 03:16:32 +0200

    change next

--- apps/nextjs-app/pages/_app.tsx
+++ apps/nextjs-app/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
-
+console.log();
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
